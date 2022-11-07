e7ee845df3f106fe50148c181a1435114329b8b9
Shilpa206 <111149272+Shilpa206@users.noreply.github.com>
2022-11-07 14:44:31 -0600

    corrected java script

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 function init(){
 
   //creating a function
-  function alertBox(){
+  function nameAlert(){
 	  
 	  // obtaining input
 	  var input = document.getElementById('entryinput');
-	  alert ("Naga Satya Silpa Annadevara:" + input);
+	  alert ("Naga Satya Silpa Annadevara: " + input.value);
 	  
 	  //obtaining output
 	  var output = document.getElementById('textoutput');
-	  output.innerHTML = input;
+	  output.innerHTML = input.value;
   }
    
    //configurating the button
-   var divButton = document. getElementById('entrybutton');
-   divbutton.addEventListener('click' , alertBox);
+   var button = document. getElementById('entrybutton');
+   button.addEventListener('click' , nameAlert);
 	  
 }
  
